324414765b60bb992d43301cc20fbcaf4d9f1e69
Marc Aradillas <aradillasm@gmail.com>
2023-06-26 13:58:35 -0500

    Added instrcutor question review/response to exercise.

--- db_tables_exercises.sql
+++ db_tables_exercises.sql
@@ -41,7 +41,7 @@ SHOW TABLES;
 -- 10. Explore the employees table. What different data types are present on this table?
 
 
-	-- char, varchar, int, date, enum
+	-- varchar/string, int/number, date/number, enum/character
 
 
 -- 11. Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
@@ -54,20 +54,34 @@ SHOW TABLES;
 
 
 	-- variable character is a variable length string data type and char store character strings. Each table in the column have either a varchar or char datatype; string value column.
+    -- ** departments, dept_emp, dept_manager, employees, titles
 
 
 -- 13. Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
 
 
-	-- All the tables have date types except for the department table
+	-- All the tables have date types except for the department table (dept_emp, dept_manager, employees, salaries, titles)
 
 
 -- 14. What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
 
 
 	-- The tables would correlate number of employees that work in respective departments and tell how long they have been emnployed there.
+    -- ** There is no direct relationship between the employees and department tables, but they do have a relationship on the dept_emp table throughout the dept_emp table.
 
 
 -- 15. Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
 
 SHOW CREATE TABLE dept_manager;
+
+-- **
+-- CREATE TABLE `dept_manager` (
+--   `emp_no` int NOT NULL,
+--   `dept_no` char(4) NOT NULL,
+--   `from_date` date NOT NULL,
+--   `to_date` date NOT NULL,
+--   PRIMARY KEY (`emp_no`,`dept_no`),
+--   KEY `dept_no` (`dept_no`),
+--   CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
+--   CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
+-- ) ENGINE=InnoDB DEFAULT CHARSET=latin1
