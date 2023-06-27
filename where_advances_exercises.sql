06388e871def3291ed06bc3af7c5aa939ae434d1
Marc Aradillas <aradillasm@gmail.com>
2023-06-27 11:32:52 -0500

    Added number to questions; Added notes for keyword uses; Added employee numbers that were missing

--- where_advances_exercises.sql
+++ where_advances_exercises.sql
@@ -7,10 +7,11 @@
 
 # ✔️
 
--- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. What is the employee number of the top three results?
+-- 1. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya' using IN. What is the employee number of the top three results?
 
 SHOW DATABASES;
-SELECT * FROM employees
+SELECT * 
+FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
 /*
@@ -21,9 +22,10 @@ Top Three results:
 #10610
 */
 
--- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. What is the employee number of the top three results? Does it match the previous question?
+-- 2. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. What is the employee number of the top three results? Does it match the previous question?
 
-SELECT * FROM employees
+SELECT * 
+FROM employees
 WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 
 /*
@@ -36,7 +38,7 @@ Top Three results:
 
 # Yes they match
 
--- Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. What is the employee number of the top three results.
+-- 3. Find all current or previous employees with first names 'Irena', 'Vidya', or 'Maya', using OR, and who is male. What is the employee number of the top three results.
 
 SELECT emp_no, first_name, gender
 FROM employees
@@ -51,35 +53,37 @@ Top Three results:
 #10821
 */
 
--- Find all unique last names that start with 'E'.
+-- 4. Find all unique last names that start with 'E'.
 
-SELECT *
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE 'E%';
 
--- Find all unique last names that start or end with 'E'.
+-- 5. Find all unique last names that start or end with 'E'.
 
-SELECT *
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 
--- Find all unique last names that end with E, but does not start with E?
+-- 6. Find all unique last names that end with E, but does not start with E?
 
-SELECT *
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name NOT LIKE 'E%' OR last_name LIKE '%E';
+# e%e also works
 
--- Find all unique last names that start and end with 'E'.
+-- 7. Find all unique last names that start and end with 'E'.
 
-SELECT *
+SELECT DISTINCT last_name
 FROM employees
-WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+WHERE last_name LIKE 'E%' AND last_name LIKE '%E'; 
 
--- Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers.
+-- 8. Find all current or previous employees hired in the 90s. Enter a comment with top three employee numbers.
 
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+# LIKE '199%' also works
 
 /*
 #10008
@@ -87,28 +91,39 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 #10012
 */
 
--- Find all current or previous employees born on Christmas. Enter a comment with top three employee numbers.
+-- 9. Find all current or previous employees born on Christmas. Enter a comment with top three employee numbers.
 
 SELECT *
 FROM employees
 WHERE birth_date LIKE '%-12-25';
 
--- Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with top three employee numbers.
+/*
+#10078
+#10115
+#10261
+*/
+-- 10. Find all current or previous employees hired in the 90s and born on Christmas. Enter a comment with top three employee numbers.
 
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 	AND birth_date LIKE '%-12-25';
 
--- Find all unique last names that have a 'q' in their last name.
+/*
+#10261
+#10438
+#10681
+*/
 
-SELECT *
+-- 11. Find all unique last names that have a 'q' in their last name.
+
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE '%q%';
 
--- Find all unique last names that have a 'q' in their last name but not 'qu'.
+-- 12. Find all unique last names that have a 'q' in their last name but not 'qu'.
 
-SELECT *
+SELECT DISTINCT last_name
 FROM employees
 WHERE last_name LIKE '%q%'
 	AND last_name NOT LIKE '%qu%';
