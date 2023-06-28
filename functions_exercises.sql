baddae9f83f01e1f03b095053ee12925bf62d03a
Marc Aradillas <aradillasm@gmail.com>
2023-06-28 11:31:29 -0500

    Updated functions exercises with complete response and comments

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -118,17 +118,48 @@ WHERE last_name LIKE 'e%e';
 -- 5. Find all employees hired in the 90s and born on Christmas. Use datediff() function to find how many days they have been working 
 --    at the company (Hint: You will also need to use NOW() or CURDATE()),
 
-SELECT *, DATEDIFF(CURDATE(), hire_daye) AS days_worked
+SELECT *, DATEDIFF(CURDATE(), hire_date) AS days_worked # NOW() also used to see query result.
 FROM employees
 WHERE hire_date LIKE '199%'
-	AND birth_date LIKE '%-12-25'
+	AND birth_date LIKE '%-12-25';
     
-
 -- 6. Find the smallest and largest current salary from the salaries table.
 
--- 7.  Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first 
+SELECT MIN(salary), MAX(salary)
+FROM salaries;
+
+# Smallest 38623
+# Largest 158220
+
+-- 7.  Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first
 -- character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits
 -- of the year that they were born. Below is an example of what the first 10 rows will look like:
+
+-- SELECT LOWER(CONCAT(SUBSTR(first_name, 1, 1), (last_name, 1, 4))) # beginning code
+
+SELECT CONCAT(
+			LOWER(
+				SUBSTR(first_name, 1, 1)), 
+			LOWER(
+				SUBSTR(last_name, 1, 4)), '_',
+			SUBSTR(birth_date, 6, 2), 
+            SUBSTR(birth_date, 3, 2)) AS user_name
+FROM employees
+LIMIT 10;
+
+/* 
+'gface_0953'
+'bsimm_0664'
+'pbamf_1259'
+'ckobl_0554'
+'kmali_0155'
+'apreu_0453'
+'tziel_0557'
+'skall_0258'
+'speac_0452'
+'dpive_0663'
+*/
+
 
 /*
 +------------+------------+-----------+------------+
