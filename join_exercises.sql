720c7558493a9609b9a5bbccf8a81072a47e9173
Marc Aradillas <aradillasm@gmail.com>
2023-06-30 11:17:24 -0500

    Added additional notes; unfinished exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,7 +1,7 @@
 -- Exercises
 -- Exercise Goals
 -- Author: Marc Aradillas
--- Date: 06-29-2023
+-- Date: 06-29-2023 - 06-30-2023
 
 
 
@@ -84,10 +84,10 @@ GROUP BY users.name, email;
 
 #INSTRUCTOR EX.
 
-SELECT roles.name, COUNT(*)
-FROM roles
-INNER JOIN users ON users.role_id = roles.id
-GROUP BY roles.name;
+-- SELECT roles.name, COUNT(*)
+-- FROM roles
+-- INNER JOIN users ON users.role_id = roles.id
+-- GROUP BY roles.name;
 
 -- Create a file named join_exercises.sql to do your work in.
 
@@ -135,13 +135,13 @@ ORDER BY Department_Name ASC;
 
 #INSTRUCTOR EX
 
-SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager' #Alias before setting alias
-FROM employees as e
-INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no # dept_emp ?
-INNER JOIN departments AS d ON de.dept_no = d.dept_no
-INNER JOIN dept_manager AS dm ON de.emp_no = dm.emp_no
-WHERE dm.to_date > CURDATE()
-ORDER BY d.dept_name ASC;
+-- SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager' #Alias before setting alias
+-- FROM employees as e
+-- INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no # dept_emp ?
+-- INNER JOIN departments AS d ON de.dept_no = d.dept_no
+-- INNER JOIN dept_manager AS dm ON de.emp_no = dm.emp_no
+-- WHERE dm.to_date > CURDATE()
+-- ORDER BY d.dept_name ASC;
 
 --   Department Name    | Department Manager
 --  --------------------+--------------------
@@ -171,14 +171,14 @@ ORDER BY departments.dept_name ASC;
 
 #INSTRUCTOR EX
 
-SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager' #Alias before setting alias
-FROM employees as e
-INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no # dept_emp ?
-INNER JOIN departments AS d ON de.dept_no = d.dept_no
-INNER JOIN dept_manager AS dm ON de.emp_no = dm.emp_no
-WHERE dm.to_date > CURDATE()
-AND e.gender = 'F'
-ORDER BY d.dept_name ASC;
+-- SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager' #Alias before setting alias
+-- FROM employees as e
+-- INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no # dept_emp ?
+-- INNER JOIN departments AS d ON de.dept_no = d.dept_no
+-- INNER JOIN dept_manager AS dm ON de.emp_no = dm.emp_no
+-- WHERE dm.to_date > CURDATE()
+-- AND e.gender = 'F'
+-- ORDER BY d.dept_name ASC;
 
 
 -- Department Name | Manager Name
@@ -205,16 +205,16 @@ ORDER BY titles.title ASC;
 
 #INSTRUCTOR EX
 
-SELECT t.title, COUNT(*) AS count
-FROM employees as e
-INNER JOIN dept_emp AS de ON de.emp_no = e.emp_no
-INNER JOIN departments AS d ON d.dept_no = de.dept_no
-INNER JOIN titles AS t ON e.emp_no = t.emp_no
-WHERE t.to_date > CURDATE()
-AND de.to_date > CURDATE()
-AND d.dept_name = 'Customer Service'
-GROUP BY t.title
-ORDER BY t.title ASC;
+-- SELECT t.title, COUNT(*) AS count
+-- FROM employees as e
+-- INNER JOIN dept_emp AS de ON de.emp_no = e.emp_no
+-- INNER JOIN departments AS d ON d.dept_no = de.dept_no
+-- INNER JOIN titles AS t ON e.emp_no = t.emp_no
+-- WHERE t.to_date > CURDATE()
+-- AND de.to_date > CURDATE()
+-- AND d.dept_name = 'Customer Service'
+-- GROUP BY t.title
+-- ORDER BY t.title ASC;
 
 
 -- Title              | Count
@@ -242,15 +242,15 @@ ORDER BY departments.dept_name ASC;
 
 #INSTRUCTOR EX
 
-SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
-FROM employees as e
-INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no # dept_emp ?
-INNER JOIN departments AS d ON de.dept_no = d.dept_no
-INNER JOIN dept_manager AS dm ON de.emp_no = dm.emp_no
-INNER JOIN salaries AS s on s.emp_no = dm.emp_no
-WHERE dm.to_date > CURDATE()
-AND s.to_date > CURDATE()
-ORDER BY d.dept_name ASC;
+-- SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS 'Salary'
+-- FROM employees as e
+-- INNER JOIN dept_emp AS de ON e.emp_no = de.emp_no # dept_emp ?
+-- INNER JOIN departments AS d ON de.dept_no = d.dept_no
+-- INNER JOIN dept_manager AS dm ON de.emp_no = dm.emp_no
+-- INNER JOIN salaries AS s on s.emp_no = dm.emp_no
+-- WHERE dm.to_date > CURDATE()
+-- AND s.to_date > CURDATE()
+-- ORDER BY d.dept_name ASC;
 
 -- Department Name    | Name              | Salary
 -- -------------------+-------------------+-------
@@ -266,7 +266,7 @@ ORDER BY d.dept_name ASC;
 
 
 
--- 6. Find the number of current employees in each department.
+-- 6. Find the number of current employees in each department. ************************* STOPPED ***************************
 
 SELECT dept_emp.dept_no AS dept_no, departments.dept_name AS dept_name, COUNT(*) AS num_employees
 FROM dept_emp
